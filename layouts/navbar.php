b1489bcffb131b0bab03db77278de05fec9c6e7a
vijayK006 <vijaykale7052@gmail.com>
2024-03-16 13:03:18 +0530

    navbar fixed

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,5 +1,30 @@
 <?php @include('layouts/topbar.php') ?>
 
-<nav>
+<nav class="d-flex gap-5 ">
+<a href="index.php" class="logo">
+    <img src="assets/img/logo/logo.jpg" alt="logo" />
+</a>
 
+<ul class="nav_links" id="menubar">
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="index.php">Home</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">About us</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Our Campuses</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Curriculum</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Gallery</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Accolades</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">New Corner</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Admission</a></li>
+</ul>
+
+<div class="menubar desktop-none" onclick='togglemenu()'>
+<i class="fa-solid fa-bars t-xlg t-color-theme-red"></i>
+</div>
 </nav>
+
+
+<script>
+    function togglemenu(){
+const menubar= document.getElementById('menubar');
+menubar.classList.toggle('openmenu')
+    }
+</script>
--- layouts/topbar.php
+++ layouts/topbar.php
@@ -1,15 +1,20 @@
 <div class="topheader-section px-5 py-2 bgcolor-theme-blue d-flex justify-content-between align-items-center">
-    <div class="enquiry-links d-flex align-items-center gap-3">
-        <a href="mailto:enrolment@vidyasagarpreschool.edu.in" class="mail t-xsm"><i class="fa-solid fa-envelope-open"></i> enrolment@vidyasagarpreschool.edu.in</a>
+    <div class="enquiry-links align-items-center gap-3">
+        <a href="mailto:enrolment@vidyasagarpreschool.edu.in" class="mail t-xsm"><i class="fa-solid fa-envelope-open"></i> &nbsp; enrolment@vidyasagarpreschool.edu.in</a>
 
-        <a href="#" class="bgcolor-theme-red px-3 py-1 b-radius-5 t-xsm ">E - Prospectus</a>
-        <a href="#" class="bgcolor-theme-red px-3 py-1 b-radius-5 t-xsm ">Enquery Now</a>
-        <a href="#" class="bgcolor-theme-red px-3 py-1 b-radius-5 t-xsm ">Contact Us</a>
-        <a href="#" class="bgcolor-theme-red px-3 py-1 b-radius-5 t-xsm ">Pay Fee</a>
+        <a href="#" class=" px-3 py-1 b-radius-5 t-xsm hover-red-btn-white-t-red transition-2 font-w-500">E - Prospectus</a>
+        <a href="#" class=" px-3 py-1 b-radius-5 t-xsm hover-red-btn-white-t-red transition-2 font-w-500">Enquery Now</a>
+        <a href="#" class=" px-3 py-1 b-radius-5 t-xsm hover-red-btn-white-t-red transition-2 font-w-500">Contact Us</a>
+        <a href="#" class=" px-3 py-1 b-radius-5 t-xsm hover-red-btn-white-t-red transition-2 font-w-500">Pay Fee</a>
 
     </div>
 
-    <div class="social-links d-flex gap-2 align-items-center">
+    <div class="social-links d-flex justify-content-md-start justify-content-between  gap-md-2  align-items-center w-sm-100">
+        <a href="">
+            <div class="socialiconbg desktop-none">
+                <i class="fa-solid fa-envelope-open icon t-color-theme-red t-xsm"></i>
+            </div>
+        </a>
 
         <a href="">
             <div class="socialiconbg">
