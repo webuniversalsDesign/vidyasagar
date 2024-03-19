542ba53a11c3f7ef554140b90fa9a2a0dde02145
webuniversalsDesign <design@webuniversals.com>
2024-03-19 10:33:13 +0530

    pushing code

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -20,7 +20,13 @@
 </li>
 
 <li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Accolades</a></li>
-<li class="t-sm py-md-1 py-3 font-w-500"><a href="#">New Corner</a></li>
+<li class="t-sm py-md-1 py-3 font-w-500 cus-dropdown">
+    <a href="#" onclick="togglesubmenu3()">News Corner &nbsp; &nbsp; <i class="fa-solid fa-chevron-right menu-arrow"></i></a>
+<ul class="drop-list" id="droplist3">
+    <li class="drop-links"><a href="blogs.php">Blogs</a> </li>
+    <li class="drop-links"><a href="sagar-times.php">Sagar Times</a></li>
+</ul>
+</li>
 <li class="t-sm py-md-1 py-3 font-w-500"><a href="#">Admission</a></li>
 </ul>
 
@@ -38,6 +44,11 @@ menubar.classList.toggle('openmenu')
 
     function togglesubmenu(){
 const droplist = document.getElementById('droplist');
+droplist.classList.toggle('opensubmenu')
+
+    }
+    function togglesubmenu3(){
+const droplist = document.getElementById('droplist3');
 droplist.classList.toggle('opensubmenu')
 
     }
